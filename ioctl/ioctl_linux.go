3ea0d3a86db0b9c5d6bfc837d3c9c7a6be6cbff3
Chris Harrington <chris.harrington@ispaceenvironments.com>
2018-06-30 09:17:38 -0500

    syscall does not return an error, it returns an errno

--- ioctl/ioctl_linux.go
+++ ioctl/ioctl_linux.go
@@ -1,11 +1,13 @@
 package ioctl
 
 import "golang.org/x/sys/unix"
+import "syscall"
 import "reflect"
 
 // Ioctl serves as a simple convenience function for invoking the
 // standard ioctl syscall using the common C convention
-func Ioctl(fd uintptr, fn int, d interface{}) (err error) {
+func Ioctl(fd uintptr, fn int, d interface{}) (error) {
+	var err syscall.Errno
 	// if nil is passed in, use a NULL pointer
 	if d == nil {
 		_, _, err = unix.Syscall(unix.SYS_IOCTL, fd, uintptr(fn), 0)
@@ -16,5 +18,8 @@ func Ioctl(fd uintptr, fn int, d interface{}) (err error) {
 		}
 		_, _, err = unix.Syscall(unix.SYS_IOCTL, fd, uintptr(fn), vo.Pointer())
 	}
-	return
+	if err != 0 {
+		return err
+	}
+	return nil
 }
