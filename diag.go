303303ceec164040dc54f41c91e0629f5e82047b
Chris Harrington <chris.harrington@ispaceenvironments.com>
2018-06-30 19:57:28 -0500

    defers don't defer evaluation... oops

--- diag.go
+++ diag.go
@@ -42,37 +42,37 @@ func main() {
 	b, err := bridge.Create("testbr0")
 	must("create a new bridge", err)
 	fmt.Fprintf(os.Stderr, "b=%+v\n", b)
-	defer must("destroy bridge", b.Destroy())
+	defer func() { must("destroy bridge", b.Destroy()) }()
 
 	t1, err := tuntap.CreateTapNamed("tap_t1")
 	must("create first tap interface", err)
 	fmt.Fprintf(os.Stderr, "t1=%+v\n", t1)
-	defer must("close t1", t1.Close())
+	defer func() { must("close t1", t1.Close()) }()
 
 	t2, err := tuntap.CreateTapNamed("tap_t2")
 	must("create second tap interface", err)
 	fmt.Fprintf(os.Stderr, "t2=%+v\n", t2)
-	defer must("close t2", t2.Close())
+	defer func() { must("close t2", t2.Close()) }()
 
 	err = b.Add(t1.NetIf())
 	must("add t1 to bridge", err)
-	defer must("remove t1 from bridge", b.Remove(t1.NetIf()))
+	defer func() { must("remove t1 from bridge", b.Remove(t1.NetIf())) }()
 
 	err = b.Add(t2.NetIf())
 	must("add t2 to bridge", err)
-	defer must("remove t2 from bridge", b.Remove(t2.NetIf()))
+	defer func() { must("remove t2 from bridge", b.Remove(t2.NetIf())) }()
 
 	err = t1.NetIf().Up()
 	must("bring up interface t1", err)
-	defer must("shut down t1", t1.NetIf().Down())
+	defer func() { must("shut down t1", t1.NetIf().Down()) }()
 
 	err = t2.NetIf().Up()
 	must("bring up interface t2", err)
-	defer must("shut down t2", t2.NetIf().Down())
+	defer func() { must("shut down t2", t2.NetIf().Down()) }()
 
 	err = b.NetIf().Up()
 	must("bring up bridge interface", err)
-	defer must("shut down bridge", b.NetIf().Down())
+	defer func() { must("shut down bridge", b.NetIf().Down()) }()
 
 	go dump("t1", t1)
 	go dump("t2", t2)
